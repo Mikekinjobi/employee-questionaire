1f7a237c017c8d3c17226e96716b95e65ab301ae
mikekinjobi <mikekinjobi@gmail.com>
2024-05-17 22:26:50 +0100

    redeploy sole manager ccf82c2f-b85a-40f2-8da8-159f5d545384

--- src/pages/CheckManagers.jsx
+++ src/pages/CheckManagers.jsx
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from "react";
-import managersData from "../data/managers.json";
-import employeesData from "../../../../Downloads/output.json";
+// import React, { useState, useEffect } from "react";
+// import managersData from "../data/managers.json";
+// import employeesData from "../../../../Downloads/output.json";
 
-export default function CheckManagers() {
-  const [managers, setManagers] = useState(managersData.allData || []);
-  const [employees, setEmployees] = useState([...new Set(employeesData)] || []);
-  const [unUsedManagers, setUnusedManagers] = useState([]);
+// export default function CheckManagers() {
+//   const [managers, setManagers] = useState(managersData.allData || []);
+//   const [employees, setEmployees] = useState([...new Set(employeesData)] || []);
+//   const [unUsedManagers, setUnusedManagers] = useState([]);
 
-  useEffect(() => {
-    setUnusedManagers(
-      managers.filter(
-        (manager) =>
-          employees.findIndex(
-            (employee) => employee.managerId === manager.answerId
-          ) === -1
-      )
-    );
-  }, [managers, employees]);
+//   useEffect(() => {
+//     setUnusedManagers(
+//       managers.filter(
+//         (manager) =>
+//           employees.findIndex(
+//             (employee) => employee.managerId === manager.answerId
+//           ) === -1
+//       )
+//     );
+//   }, [managers, employees]);
 
-  return (
-    <div>
-      <ul>
-        {unUsedManagers.map((manager) => (
-          <li key={manager.answerId}>{manager.answerId}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <ul>
+//         {unUsedManagers.map((manager) => (
+//           <li key={manager.answerId}>{manager.answerId}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
